4b409151bef2a313114168bc56a5a9f641719a40
Alexandre Frazão <alexf.r_kim@hotmail.com>
2019-02-06 16:26:34 +0000

    Corrected structure format

--- Toy-Brain/toy_brain/toy_brain.h
+++ Toy-Brain/toy_brain/toy_brain.h
@@ -1,4 +1,4 @@
-﻿// toy-brain.h : Include file for standard system include files,
+﻿// toy_brain.h : Include file for standard system include files,
 // or project specific include files.
 
 #pragma once
