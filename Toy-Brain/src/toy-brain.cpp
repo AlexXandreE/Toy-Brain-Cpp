5f1c8772421ecf4d1186f88a628dbc4ca22a76bf
Alexandre Frazão <alexf.r_kim@hotmail.com>
2019-02-06 16:06:12 +0000

    Initial project structure

---
+++ Toy-Brain/src/toy-brain.cpp
@@ -0,0 +1,12 @@
+﻿// toy-brain.cpp : Defines the entry point for the application.
+//
+
+#include "toy-brain.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
